39d7e14fc2cde074ff2ddeba406fba68821201c2
Mmanare Mahlatse nkoana <mahlatsemmanarenkoana@gmail.com>
2024-09-23 17:26:27 +0200

    ongoing changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,9 @@
 class BookingsController < ApplicationController
+  def index
+    @my_bookings = Booking.where(user_id: current_user.id)
+    @my_goods = current_user.goods
+
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "bookings", to: "bookings#index"
 end
